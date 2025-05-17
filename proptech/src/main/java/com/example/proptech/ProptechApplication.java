c26b52564497192c8f1bd16781d3ac426e7cd744
Thinhskyduck <160042245+Thinhskyduck@users.noreply.github.com>
2025-05-17 17:05:22 +0700

    Initial commit

---
+++ proptech/src/main/java/com/example/proptech/ProptechApplication.java
@@ -0,0 +1,13 @@
+package com.example.proptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProptechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProptechApplication.class, args);
+	}
+
+}
---
+++ proptech/src/test/java/com/example/proptech/ProptechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProptechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
